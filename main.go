01aa39400d3fd5af93bf51ad3e891bb0b58f276f
Dysar <david.zingerman@tptlive.ee>
2016-10-14 17:16:20 +0300

    mazafaka

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 9
+	fmt.Println(Result(a,b))
+}
+
+func Result(a int, b int)(g int){
+	g = a + b
+	//fmt.Println("a + b =", g)
+	return
+}
---
+++ main_test.go
@@ -0,0 +1,18 @@
+package main_test
+
+import (
+	"testing"
+	ciproject "../CIproject"
+)
+
+func TestResult(t *testing.T) {
+	if ciproject.Result(20, 15) > 50 {
+		t.Error("GG man")
+	}
+}
+
+func TestResultFail(t *testing.T) {
+	if ciproject.Result(20, 15) < 50 {
+		t.Error("GG man")
+	}
+}
