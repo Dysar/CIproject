6b3368c55b1fed9f665d8f50f9e8178885926281
Dysar <david.zingerman@tptlive.ee>
2016-10-21 17:52:14 +0300

    Mazafako

--- ci/HookServe.go
+++ ci/HookServe.go
@@ -19,6 +19,9 @@ func main() {
 	// Everytime the server receives a webhook event, print the results
 	for event := range server.Events {
 		fmt.Println(event.Owner + " " + event.Repo + " " + event.Branch + " " + event.Commit)
+		if err := os.Chdir("/home/ubuntu"); err != nil {
+			panic(err)
+		}
 		err := os.Mkdir(event.Commit, 755) //create clone repo for this commit
 		if err != nil {
 			panic(err)
@@ -79,12 +82,14 @@ func gitpreparation(){
 	if _, err := exec.Command(cmdName, cmdArgs...).Output(); err != nil {
 		fmt.Fprintln(os.Stderr, "Repository already exits", err)
 	}
+
 	cmdName2 := "git"
 	cmdArgs2 := []string {"fetch", "https://github.com/Dysar/CIproject"} //git fetch
 	if _, err := exec.Command(cmdName2, cmdArgs2...).Output(); err != nil {
 		fmt.Fprintln(os.Stderr, "There was an error running git fetch command: ", err)
 		os.Exit(1)
 	}
+
 }
 func gitcheckout(hash string){
 	cmdName := "git"
